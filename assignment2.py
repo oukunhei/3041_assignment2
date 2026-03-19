016fd7e514e49111d8bec9d55fc637a8175dee0e
oukunhei <okhou533@connect.hkust-gz.edu.cn>
2026-03-19 21:58:11 +0800

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,24 @@
+# use Euclidean distance as heuristic function
+import sys
+import heapq
+from typing import List, Dict, Tuple
+from pathlib import Path
+
+def BFS(start: list) -> int:
+
+    return path_exist, length, expanded_states, time
+
+
+
+
+def a_star_0(start: list) -> int:
+    
+    return path_exist, length, expanded_states, time
+    
+            
+def a_star_1(start: list) -> int:
+    
+    return path_exist, length, expanded_states, time
+
+if __name__ == "__main__":
+    
